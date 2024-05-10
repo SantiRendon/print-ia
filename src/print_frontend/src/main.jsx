a3fd2d19ba1bcf01f571a9197ba912e53cb013e6
santiRendon <xantirendonc@gmail.com>
2024-05-10 09:43:51 -0500

    🔧 configuration react-router-dom

--- src/print_frontend/src/main.jsx
+++ src/print_frontend/src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.scss';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/auth",
+    element: <h1>Auth</h1>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
