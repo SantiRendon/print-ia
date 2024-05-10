9681fc9b481dd3bc480860d8bb79cc3d1c8e07a1
santiRendon <xantirendonc@gmail.com>
2024-05-10 12:34:11 -0500

    ♻️ refactor verificationButton

--- src/print_frontend/src/components/VerificationButton/index.jsx
+++ src/print_frontend/src/components/VerificationButton/index.jsx
@@ -1,9 +1,9 @@
 import { Button } from "@material-tailwind/react";
- 
+
 export default function VerificationButton() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    );
 }
