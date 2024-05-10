4342e78d7ec9236798b567006b2259dd0eb1aec1
santiRendon <xantirendonc@gmail.com>
2024-05-10 10:04:32 -0500

    💄 added verificationButton component

---
+++ src/print_frontend/src/components/VerificationButton/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+const VerificationButton = () => {
+  return (
+    <button className='verify-class'>Verify with printIA</button>
+  )
+}
+
+export default VerificationButton
